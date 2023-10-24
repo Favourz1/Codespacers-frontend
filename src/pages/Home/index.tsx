a75304bffa465874cc59e6b633ff3ceeb898c1a0
Favour Okoh <okohfavour91@gmail.com>
2023-10-24 08:26:13 +0100

    Feat: Unique Feature Section

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,6 +2,7 @@ import Button from "@/components/Button"
 import heroWomanImg from "@/assets/pngs/hero-woman.png"
 import whoWeAreImg from "@/assets/pngs/who-we-are-girl.png"
 import ChevronRight from "@/assets/icons/chevron-right.svg"
+import { UNIQUE_FEATURES } from "@/lib/constants"
 const Home = () => {
     return (
         <div>
@@ -19,16 +20,41 @@ const Home = () => {
                 </div>
             </section>
             {/* Who We are */}
-            <section className="py-8 bg-bgWhite">
+            <section className="py-8">
                 <div className="container mx-auto px-3">
-                    <div className="flex flex-col gap-12 md:flex-row">
-                        <div className="flex flex-col w-full md:w-[40%] space-y-5">
+                    <div className="flex flex-col md:flex-row">
+                        <div className="flex flex-col justify-center w-full md:w-[40%] space-y-5">
                             <div className="text-3xl text-primaryColor font-semibold">Who We Are</div>
                             <p className="text-primaryAsh">At Budgetify, we are passionate about helping you take control of your finances. Our mission is to simplify budgeting and financial planning and also track expenses making it accessible to everyone.</p>
                             <Button customClass="max-w-[60%]" variant="filled" iconPlacement="end" icon={<ChevronRight />}>Learn More</Button>
                         </div>
                         <div className="w-full md:w-[60%] flex items-center relative">
                             <img src={whoWeAreImg} alt="" />
+                        </div>
+                    </div>
+                </div>
+            </section>
+            {/* Unique Features */}
+            <section className="py-8">
+                <div className="container mx-auto px-3">
+                    <div className="flex flex-col">
+                        <div className="max-w-[50%] justify-center items-center text-center mx-auto mb-5">
+                            <div className="text-3xl text-primaryColor font-semibold">Our Unique Features</div>
+                            <p className="text-primaryAsh">Our web app has you in mind. We believe that managing your money is important and should be easy stress-free.</p>
+                        </div>
+                        <div className="grid grid-cols-2 gap-12">
+                            {
+                                UNIQUE_FEATURES.map(item => (
+                                    <div key={item.id} className="flex items-center rounded-lg shadow-md gap-[10px] p-5">
+                                        {item.icon}
+                                        <div className="space-y-2">
+                                            <div className="text-xl font-medium">{item.headingText}</div>
+                                            <div className="text-primaryAsh text-sm">{item.subText}</div>
+                                        </div>
+                                    </div>
+                                ))
+                            }
+
                         </div>
                     </div>
                 </div>
